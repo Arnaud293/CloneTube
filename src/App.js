e141d0ccce619bd9b6b30380c4d61155ddb6242f
Arnaud293 <arnaudlt293@gmail.com>
2022-12-20 11:58:33 +0100

    create empty components and import them

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Box } from '@mui/material';
 
+import Navbar from './components/Navbar';
+import Feed from './components/Feed';
+import VideoDetails from './components/VideoDetails';
+import ChannelDetails from './components/ChannelDetails';
+import SearchFeed from './components/SearchFeed'
+
 const App = () => (
   <BrowserRouter>
     <Box sx={{backgroundColor: "black"}}>
