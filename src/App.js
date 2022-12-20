664fb0826012860ea483034fd450669cb0d3cdf1
Arnaud293 <arnaudlt293@gmail.com>
2022-12-20 11:54:30 +0100

    preparing app structure/routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Box } from '@mui/material';
 
-const App = () => {
-  return (
-    <div>
-      Application
-    </div>
-  );
-};
+const App = () => (
+  <BrowserRouter>
+    <Box sx={{backgroundColor: "black"}}>
+      <Navbar />
+      <Routes>
+        <Route to='/' element={<Feed/>} />
+        <Route to='/video/:id' element={<VideoDetails/>} />
+        <Route to='/channel/:id' element={<ChannelDetails/>} />
+        <Route to='/search/:searchTerm' element={<SearchFeed/>} />
+      </Routes>
+    </Box>
+  </BrowserRouter>
+)
+;
 
 export default App;
