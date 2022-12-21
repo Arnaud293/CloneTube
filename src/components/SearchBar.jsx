050b43138f2a7125a31f5800a3f020683749d125
Arnaud293 <arnaudlt293@gmail.com>
2022-12-21 11:02:08 +0100

    submit form on search, pass the query to url

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -5,9 +5,23 @@ import { Paper, IconButton } from '@mui/material';
 import {Search} from '@mui/icons-material'
 
 const SearchBar = () => {
+
+  const [searchTerms, setSearchTerms] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if(searchTerms){
+      navigate(`/search/${searchTerms}`);
+
+      setSearchTerms('');
+    }
+  }
+
   return (
-    <Paper component="form" onSubmit={() => {}} sx={{borderRadius: 20, border: "1px solid #e3e3e3", pl: 2, boxShadow : 'none', mr: {sm:5}}} >
-        <input className='search-bar' placeholder='Search...' value='' onChange={() => {}} />
+    <Paper component="form" onSubmit={handleSubmit} sx={{borderRadius: 20, border: "1px solid #e3e3e3", pl: 2, boxShadow : 'none', mr: {sm:5}}} >
+        <input className='search-bar' placeholder='Search...' value={searchTerms} onChange={(e) => {setSearchTerms(e.target.value)}} />
         <IconButton type='submit' sx={{p:'10px', color:'black'}} >
             <Search/>
         </IconButton>
